d95efdd52f8188e98c1dceca4df7f84d40b086c5
han8909227 <hbao2016@hotmail.com>
2017-07-17 17:22:28 -0700

    finalized

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 'use strict';
 
-
-
 var time = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 
 //object for a specific location of sale
@@ -90,8 +88,6 @@ var pike = {
     }
 
   };
-
-
 
 pike.sumCust();
 pike.sumCook();
@@ -381,8 +377,6 @@ caphill.render();
 caphill.sumCust();
 caphill.sumCook();
 caphill.total();
-
-
 
 
 
