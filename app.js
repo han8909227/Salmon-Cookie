53b07660a356697b367d6970bbe7575f7c5cdd27
han8909227 <hbao2016@hotmail.com>
2017-07-18 15:16:27 -0700

    table operational

--- app.js
+++ app.js
@@ -2,472 +2,95 @@
 
 var time = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 
-//object for a specific location of sale
-var pike = {
+var shopObj = [];
+var shopLoc = ['1st and Pike','SeaTac Airport','Seattle Center','Capitol Hill','Alki'];
+var salesTable = document.getElementById('sales');
+makeHeaderRow();
 
-  name: 'pike',
-  minCust: 23,
-  maxCust: 65,
-  avgCook: 6.3,
-  custEachHr: [],
-  cookEachHr: [],
 
+new Sales('1st and Pike',23,65,6.3);
+new Sales('SeaTac Airport',3,24,1.2);
+new Sales('Seattle Center',11,38,3.7);
+new Sales('Capitol Hill',20,38,2.3);
+new Sales('Alki',2,16,4.6);
+
+for(var i = 0; i < shopLoc.length; i++){
+  shopObj[i].cookPerSale();
+}
+
+for(i = 0; i < shopLoc.length; i++){
+  shopObj[i].render();
+}
+
+function Sales(name,minCust,maxCust,avgCook) {
+
+  this.name = name;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCook = avgCook;
+  this.totalCook = 0;
+  this.custEachHr = [];
+  this.cookEachHr = [];
 
 //random num generates # of cust in between the min/max customer per hr and push into an array
-  custPerHr: function(){
-    for(var i=0;i<time.length;i++){
+  this.custPerHr = function(){
+    for(var i = 0; i < time.length; i++){
       this.custEachHr.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
     }
-},
-
-
-//generate the number of sale for each day and push into an array
-  cookPerSale: function(){
-
-      this.custPerHr();
-      for(var i=0;i<time.length;i++){
-        this.cookEachHr.push(Math.floor(this.custEachHr[i] * this.avgCook));
-
-      }
-  },
-
-
-  //render
-  render: function(){
-    var pikeUL = document.getElementById('pike');
-    // This will render the seagull data array to the DOM
-    // We'll need a 'for' loop to iterate over the array
-    for(var i = 0; i < this.cookEachHr.length; i++){
-      // There are three parts to this process:
-
-      // 1. Create an element
-      var liEl = document.createElement('li');
-
-      // 2. Give it content
-      liEl.textContent = time[i] + ':  ' + this.cookEachHr[i];
-
-
-      // 3. Append it to a certain place in the DOM
-      // parentElement.appendChild(childElement)
-      pikeUL.appendChild(liEl);
-    }
-  },
-
-  sum: function(a,b){
-    return a+b;
-  },
-
-  sumCust: function(){
-    var totalCust = 0;
-    for(var i=0;i<this.custEachHr.length;i++){
-
-      totalCust = this.sum(totalCust,this.custEachHr[i]);
-
-    }
-
-    return totalCust;
-
-  },
-
-  sumCook: function(){
-    var totalCook = 0;
-    for(var i=0;i<this.cookEachHr.length;i++){
-
-      totalCook = this.sum(totalCook,this.cookEachHr[i]);
-
-    }
-    return totalCook;
-  },
-
-
-  total: function(){
-    var pikeUL = document.getElementById('pike');
-      var liEl = document.createElement('li');
-      liEl.textContent = 'Total' + ':  ' + this.sumCook();
-      pikeUL.appendChild(liEl);
-    }
-
   };
 
-pike.sumCust();
-pike.sumCook();
-pike.cookPerSale();
-pike.render();
-pike.total();
-
-
-
-
-//object for a specific location of sale
-var seatac = {
-
-  name: 'seatac',
-  minCust: 3,
-  maxCust: 24,
-  avgCook: 1.2,
-  custEachHr: [],
-  cookEachHr: [],
-
-
-//random num generates # of cust in between the min/max customer per hr and push into an array
-  custPerHr: function(){
-    for(var i=0;i<time.length;i++){
-      this.custEachHr.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
-    }
-},
-
 
 //generate the number of sale for each day and push into an array
-  cookPerSale: function(){
-
-      this.custPerHr();
-      for(var i=0;i<time.length;i++){
-        this.cookEachHr.push(Math.floor(this.custEachHr[i] * this.avgCook));
-
-      }
-  },
-
-
-  //render
-  render: function(){
-    var seatacUL = document.getElementById('seatac');
-    // This will render the seagull data array to the DOM
-    // We'll need a 'for' loop to iterate over the array
-    for(var i = 0; i < this.cookEachHr.length; i++){
-      // There are three parts to this process:
-
-      // 1. Create an element
-      var liEl = document.createElement('li');
-
-      // 2. Give it content
-      liEl.textContent = time[i] + ':  ' + this.cookEachHr[i];
-
-      // 3. Append it to a certain place in the DOM
-      // parentElement.appendChild(childElement)
-      seatacUL.appendChild(liEl);
+  this.cookPerSale = function(){
+    this.custPerHr();
+    for(var i = 0; i < time.length; i++){
+      this.cookEachHr.push(Math.ceil(this.custEachHr[i] * this.avgCook));
+      this.totalCook += this.cookEachHr[i];
     }
-  },
+  };
 
-  sum: function(a,b){
-    return a+b;
-  },
 
-  sumCust: function(){
-    var totalCust = 0;
-    for(var i=0;i<this.custEachHr.length;i++){
+  this.render = function(){
+    var trEl = document.createElement('tr'); // create the row element
 
+    var thEl = document.createElement('th'); // create loc info
+    thEl.textContent = this.name;
+    trEl.appendChild(thEl);
+    salesTable.appendChild(trEl);
 
-      totalCust = this.sum(totalCust,this.custEachHr[i]);
-    }
 
-    return totalCust;
+    for(var i = 0; i < time.length; i++){
+      var tdEl = document.createElement('td');
+      tdEl.textContent = this.cookEachHr[i];
+      trEl.appendChild(tdEl);
+      salesTable.appendChild(trEl);
 
-  },
+    }};
 
-  sumCook: function(){
-    var totalCook = 0;
-    for(var i=0;i<this.cookEachHr.length;i++){
+  shopObj.push(this);
+}
 
 
-      totalCook = this.sum(totalCook,this.cookEachHr[i]);
-    }
+function makeHeaderRow(){
+  var trEl = document.createElement('tr');  //create a row
 
-    return totalCook;
+  //spacer
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Loc/Time ';
+  trEl.appendChild(thEl);
+  salesTable.appendChild(trEl);
 
-  },
+  for(var i = 0; i < time.length; i++){
+    thEl = document.createElement('th');
+    thEl.textContent = time[i];
+    trEl.appendChild(thEl);
+    salesTable.appendChild(trEl);
+  }
+}
 
-  total: function(){
-    var seatacUl = document.getElementById('seatac');
-      var liEl = document.createElement('li');
-      liEl.textContent = 'Total' + ':  ' + this.sumCook();
-      seatacUl.appendChild(liEl);
-    }
-};
 
-seatac.cookPerSale();
-seatac.render();
-seatac.sumCust();
-seatac.sumCook();
-seatac.total();
 
 
 
-//object for a specific location of sale
-var seacenter = {
 
-  name: 'seacenter',
-  minCust: 11,
-  maxCust: 38,
-  avgCook: 3.7,
-  custEachHr: [],
-  cookEachHr: [],
-
-
-//random num generates # of cust in between the min/max customer per hr and push into an array
-  custPerHr: function(){
-    for(var i=0;i<time.length;i++){
-      this.custEachHr.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
-    }
-},
-
-
-//generate the number of sale for each day and push into an array
-  cookPerSale: function(){
-
-      this.custPerHr();
-      for(var i=0;i<time.length;i++){
-        this.cookEachHr.push(Math.floor(this.custEachHr[i] * this.avgCook));
-
-      }
-  },
-
-
-  //render
-  render: function(){
-    var seacenterUL = document.getElementById('seacenter');
-    // This will render the seagull data array to the DOM
-    // We'll need a 'for' loop to iterate over the array
-    for(var i = 0; i < this.cookEachHr.length; i++){
-      // There are three parts to this process:
-
-      // 1. Create an element
-      var liEl = document.createElement('li');
-
-      // 2. Give it content
-      liEl.textContent = time[i] + ':  ' + this.cookEachHr[i];
-
-      // 3. Append it to a certain place in the DOM
-      // parentElement.appendChild(childElement)
-      seacenterUL.appendChild(liEl);
-    }
-  },
-
-  sum: function(a,b){
-    return a+b;
-  },
-
-  sumCust: function(){
-    var totalCust = 0;
-    for(var i=0;i<this.custEachHr.length;i++){
-
-
-      totalCust = this.sum(totalCust,this.custEachHr[i]);
-    }
-
-    return totalCust;
-
-  },
-
-  sumCook: function(){
-    var totalCook = 0;
-    for(var i=0;i<this.cookEachHr.length;i++){
-
-
-      totalCook = this.sum(totalCook,this.cookEachHr[i]);
-    }
-
-    return totalCook;
-
-  },
-
-  total: function(){
-    var seacenterUL = document.getElementById('seacenter');
-      var liEl = document.createElement('li');
-      liEl.textContent = 'Total' + ':  ' + this.sumCook();
-      seacenterUL.appendChild(liEl);
-    }
-};
-
-seacenter.cookPerSale();
-seacenter.render();
-seacenter.sumCust();
-seacenter.sumCook();
-seacenter.total();
-
-
-
-//object for a specific location of sale
-var caphill = {
-
-  name: 'caphill',
-  minCust: 20,
-  maxCust: 38,
-  avgCook: 2.3,
-  custEachHr: [],
-  cookEachHr: [],
-
-
-//random num generates # of cust in between the min/max customer per hr and push into an array
-  custPerHr: function(){
-    for(var i=0;i<time.length;i++){
-      this.custEachHr.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
-    }
-},
-
-
-//generate the number of sale for each day and push into an array
-  cookPerSale: function(){
-
-      this.custPerHr();
-      for(var i=0;i<time.length;i++){
-        this.cookEachHr.push(Math.floor(this.custEachHr[i] * this.avgCook));
-
-      }
-  },
-
-
-  //render
-  render: function(){
-    var caphillUL = document.getElementById('caphill');
-    // This will render the seagull data array to the DOM
-    // We'll need a 'for' loop to iterate over the array
-    for(var i = 0; i < this.cookEachHr.length; i++){
-      // There are three parts to this process:
-
-      // 1. Create an element
-      var liEl = document.createElement('li');
-
-      // 2. Give it content
-      liEl.textContent = time[i] + ':  ' + this.cookEachHr[i];
-
-      // 3. Append it to a certain place in the DOM
-      // parentElement.appendChild(childElement)
-      caphillUL.appendChild(liEl);
-    }
-  },
-
-  sum: function(a,b){
-    return a+b;
-  },
-
-  sumCust: function(){
-    var totalCust = 0;
-    for(var i=0;i<this.custEachHr.length;i++){
-
-
-      totalCust = this.sum(totalCust,this.custEachHr[i]);
-    }
-
-    return totalCust;
-
-  },
-
-  sumCook: function(){
-    var totalCook = 0;
-    for(var i=0;i<this.cookEachHr.length;i++){
-
-
-      totalCook = this.sum(totalCook,this.cookEachHr[i]);
-    }
-
-    return totalCook;
-
-  },
-
-  total: function(){
-    var caphillUL = document.getElementById('caphill');
-      var liEl = document.createElement('li');
-      liEl.textContent = 'Total' + ':  ' + this.sumCook();
-      caphillUL.appendChild(liEl);
-    }
-};
-
-caphill.cookPerSale();
-caphill.render();
-caphill.sumCust();
-caphill.sumCook();
-caphill.total();
-
-
-
-//object for a specific location of sale
-var alki = {
-
-  name: 'alki',
-  minCust: 2,
-  maxCust: 16,
-  avgCook: 4.6,
-  custEachHr: [],
-  cookEachHr: [],
-
-
-//random num generates # of cust in between the min/max customer per hr and push into an array
-  custPerHr: function(){
-    for(var i=0;i<time.length;i++){
-      this.custEachHr.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
-    }
-},
-
-
-//generate the number of sale for each day and push into an array
-  cookPerSale: function(){
-
-      this.custPerHr();
-      for(var i=0;i<time.length;i++){
-        this.cookEachHr.push(Math.floor(this.custEachHr[i] * this.avgCook));
-
-      }
-  },
-
-
-  //render
-  render: function(){
-    var alkiUL = document.getElementById('alki');
-    // This will render the seagull data array to the DOM
-    // We'll need a 'for' loop to iterate over the array
-    for(var i = 0; i < this.cookEachHr.length; i++){
-      // There are three parts to this process:
-
-      // 1. Create an element
-      var liEl = document.createElement('li');
-
-      // 2. Give it content
-      liEl.textContent = time[i] + ':  ' + this.cookEachHr[i];
-
-      // 3. Append it to a certain place in the DOM
-      // parentElement.appendChild(childElement)
-      alkiUL.appendChild(liEl);
-    }
-  },
-
-  sum: function(a,b){
-    return a+b;
-  },
-
-  sumCust: function(){
-    var totalCust = 0;
-    for(var i=0;i<this.custEachHr.length;i++){
-
-
-      totalCust = this.sum(totalCust,this.custEachHr[i]);
-    }
-
-    return totalCust;
-
-  },
-
-  sumCook: function(){
-    var totalCook = 0;
-    for(var i=0;i<this.cookEachHr.length;i++){
-
-
-      totalCook = this.sum(totalCook,this.cookEachHr[i]);
-    }
-
-    return totalCook;
-
-  },
-
-  total: function(){
-    var alkiUL = document.getElementById('alki');
-      var liEl = document.createElement('li');
-      liEl.textContent = 'Total' + ':  ' + this.sumCook();
-      alkiUL.appendChild(liEl);
-    }
-};
-
-alki.cookPerSale();
-alki.render();
-alki.sumCust();
-alki.sumCook();
-alki.total();
+//end
